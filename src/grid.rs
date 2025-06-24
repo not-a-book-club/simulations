c732b6af967cc33b6bc2fc362244a20a301fb5cf
Chris <chrisbutler296@gmail.com>
2025-06-23 21:06:13 -0600

    Add sketchy Grid trait unit test

--- src/grid.rs
+++ src/grid.rs
@@ -47,3 +47,94 @@ pub trait Grid: Sized {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::BitFlipper;
+
+    use super::*;
+
+    #[derive(Debug, PartialEq, Eq)]
+    struct TestGridWithMut<'a, G: Grid> {
+        grid: G,
+        flipped: &'a mut Vec<(Index, Index)>,
+    }
+
+    impl<G: Grid> Grid for TestGridWithMut<'_, G> {
+        fn new(width: usize, height: usize) -> Self {
+            unreachable!(
+                "Not expected to be called by bitflipper: new(width: {width}, height: {height})"
+            );
+        }
+
+        fn width(&self) -> Index {
+            self.grid.width()
+        }
+
+        fn height(&self) -> Index {
+            self.grid.height()
+        }
+
+        fn get(&self, x: Index, y: Index) -> bool {
+            let elem = self.grid.get(x, y);
+            unreachable!("Not expected to be called by bitflipper: get(x: {x}, y: {y}) == {elem}");
+        }
+
+        fn set(&mut self, x: Index, y: Index, elem: bool) -> bool {
+            unreachable!(
+                "Not expected to be called by bitflipper: set(x: {x}, y: {y}, elem: {elem})"
+            );
+        }
+
+        fn flip(&mut self, x: Index, y: Index) -> bool {
+            dbg!((x, y));
+            self.flipped.push((x, y));
+            self.grid.flip(x, y)
+        }
+    }
+
+    // Make sure certain types of Grid impls are possible
+    #[test]
+    fn check_flip_tracking_with_mut() {
+        let bitgrid = crate::BitGrid::new(32, 32);
+        let mut flipped = vec![];
+        let mut bitflipper = BitFlipper::new_with_grid(
+            TestGridWithMut {
+                grid: bitgrid,
+                flipped: &mut flipped,
+            },
+            1, // dx
+            1, // dy
+        );
+
+        // Note: Must use grid()/grid_mut() because:
+        //      error[E0502]: cannot borrow `flipped` as immutable because it is also borrowed as mutable
+        //         --> src/grid.rs:111:14
+        //          |
+        //      105 |                 flipped: &mut flipped,
+        //          |                          ------------ mutable borrow occurs here
+        //      ...
+        //      111 |         dbg!(&flipped);
+        //          |              ^^^^^^^^ immutable borrow occurs here
+        //      112 |
+        //      113 |         bitflipper.flip_and_advance(1);
+        //          |         ---------- mutable borrow later used here
+        // dbg!(&flipped);
+
+        bitflipper.flip_and_advance(1);
+        assert_eq!(bitflipper.grid().flipped, &[(31, 31)]);
+        bitflipper.grid_mut().flipped.clear();
+
+        bitflipper.flip_and_advance(1);
+        assert_eq!(bitflipper.grid().flipped, &[(30, 30)]);
+        bitflipper.grid_mut().flipped.clear();
+
+        bitflipper.flip_and_advance(1);
+        assert_eq!(bitflipper.grid().flipped, &[(29, 29)]);
+        bitflipper.grid_mut().flipped.clear();
+
+        bitflipper.flip_and_advance(1);
+        assert_eq!(bitflipper.grid().flipped, &[(28, 28)]);
+        bitflipper.grid_mut().flipped.clear();
+    }
+}
